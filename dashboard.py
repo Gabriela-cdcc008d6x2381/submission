f9892f39a4c2f91e5c2afd3a649d918fa6fc4737
Gabriela-cdcc008d6x2381 <cdcc008d6x2381@student.devacademy.id>
2026-04-08 19:34:45 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Bike Sharing Dashboard", layout="wide")
 
-df = pd.read_csv("C:\\Users\\X1 PRO\\Downloads\\dicoding\\Coding Camp\\Fundamental Analisa Data\\Submission\\dashboard\\main_data.csv")
+df = pd.read_csv("main_data.csv")
 df['date'] = pd.to_datetime(df['date'])
 
 weather_map = {
